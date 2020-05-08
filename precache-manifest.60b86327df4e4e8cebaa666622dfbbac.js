b0295ac6f3327a6499b5f192e8ce46e9e35ea4a0
RenanBrayner <renanbrayner@gmail.com>
2020-05-08 19:09:18 -0300

    Updates

---
+++ precache-manifest.60b86327df4e4e8cebaa666622dfbbac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175651311e2cf4585ef2ca01b2074cda",
+    "url": "/sorteador/index.html"
+  },
+  {
+    "revision": "c3219a843680e240b7e6",
+    "url": "/sorteador/static/css/main.835ed58a.chunk.css"
+  },
+  {
+    "revision": "683ab8b54d9cb81d33de",
+    "url": "/sorteador/static/js/2.307b981d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorteador/static/js/2.307b981d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3219a843680e240b7e6",
+    "url": "/sorteador/static/js/main.4a3fb58e.chunk.js"
+  },
+  {
+    "revision": "e482a0117b26a5b902f0",
+    "url": "/sorteador/static/js/runtime-main.3487f852.js"
+  }
+]);
